3add66d491e9aa76c945ad8b6acc7afd2fc93e4e
Olaide Ojewale <ojewaleolaide@gmail.com>
2016-06-01 07:20:50 +0100

    Add basic properties to Member and Book/

---
+++ src/main/java/readers/Book.java
@@ -0,0 +1,8 @@
+package readers;
+
+public class Book {
+    private String Bookname;
+    private String Author;
+    private String NumberOfCopies;
+    private String IsbnNumber;
+}
--- src/main/java/readers/Member.java
+++ src/main/java/readers/Member.java
@@ -3,4 +3,78 @@ import java.util.Date;
 
 public class Member {
 
+    private String fullname;
+    private String dateOfBirth;
+    private String phoneNumber;
+    private String email;
+    private char gender;
+    private Date dateOfReg;
+
+    public Member() {
+    }
+
+    public Member(String fullname, char gender, String dateOfBirth, String phoneNumber, String email) {
+        this.fullname = fullname;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.dateOfReg = new Date();
+    }
+
+    public void borrowBook(Book book) {
+
+    }
+
+    public Book returnBook(Book book) {
+        return book;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfReg() {
+        return dateOfReg;
+    }
+
+    public void setDateOfReg(Date dateOfReg) {
+        this.dateOfReg = dateOfReg;
+    }
 }
